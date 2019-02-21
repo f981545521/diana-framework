5994577eee900e001be8c829636546443b53217f
youfang <981545521@qq.com>
2019-02-21 09:57:00 +0800

    add: diana gateway import

--- diana-gateway/src/main/java/cn/acyou/diana/gateway/DianaGatewayApplication.java
+++ diana-gateway/src/main/java/cn/acyou/diana/gateway/DianaGatewayApplication.java
@@ -3,9 +3,7 @@ package cn.acyou.diana.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @EnableEurekaClient
 @SpringBootApplication
 public class DianaGatewayApplication {
